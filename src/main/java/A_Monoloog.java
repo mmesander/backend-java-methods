c9c031789299e8981d5639dd3d9ef0ecd5dfec35
Mark Mesander <mark_mesander@hotmail.com>
2023-08-30 12:08:20 +0200

    complete A

--- src/main/java/A_Monoloog.java
+++ src/main/java/A_Monoloog.java
@@ -3,10 +3,22 @@ public class A_Monoloog {
     public static void main(String[] args) {
         // Groepeer de statement in hun eigen method, en roep de method aan vanuit main. De code blijft functioneel hetzelfde doen.
         // Een conversatie bestaat uit een `greeting`, wat `smallTalk` en een `goodbye`
+        greeting();
+        smallTalk();
+        goodbye();
+    }
+
+    private static void greeting() {
         System.out.println("Hey");
         System.out.println("Welcome to this wonderful conversation");
+    }
+
+    private static void smallTalk() {
         System.out.println("How about this weather?");
         System.out.println("And did you see that random program on that random channel?");
+    }
+
+    private static void goodbye() {
         System.out.println("Thank you for a wonderful conversation!");
         System.out.println("Cya");
     }
